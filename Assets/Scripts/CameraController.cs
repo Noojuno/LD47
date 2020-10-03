347ba6eba365890bfea11b16b75fe69f10f9cf36
Jono Kemball <Noojuno@users.noreply.github.com>
2020-10-03 14:46:42 +1300

    basic movement + textmesh pro

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public float cameraFollowSpeed = 0.3f;
+    public float mouseOffsetMultiplier = 1f;
+    public Vector3 offset;
+    public Texture2D cursor;
+
+    public Vector3 mouseOffset = Vector2.zero;
+
+    void Update()
+    {
+        Cursor.SetCursor(this.cursor, Vector2.zero, CursorMode.Auto);
+
+        this.mouseOffset = this.CalculateMouseOffset();
+    }
+
+    Vector3 CalculateMouseOffset()
+    {
+        Vector2 ret = Camera.main.ScreenToViewportPoint(Input.mousePosition); //raw mouse pos
+        ret *= 2;
+        ret -= Vector2.one; //set (0,0) of mouse to middle of screen
+        float max = 0.9f;
+
+        if (Mathf.Abs(ret.x) > max || Mathf.Abs(ret.y) > max)
+        {
+            ret = ret.normalized; //helps smooth near edges of screen
+        }
+
+        return ret * this.mouseOffsetMultiplier;
+    }
+
+    void FixedUpdate()
+    {
+        if (this.target != null)
+        {
+            Vector3 targetPosition = target.position + offset;
+            this.transform.position = Vector3.Lerp(this.transform.position, targetPosition + this.offset + this.mouseOffset, cameraFollowSpeed);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator), typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+    public Rigidbody2D rigidBody;
+    public Animator animator;
+
+    public bool IsWalking => this.movement.magnitude > 0;
+
+    private Vector2 movement;
+    private Vector2 previousMovement;
+    private bool facingRight = true;
+
+    void Awake()
+    {
+        this.rigidBody = this.GetComponent<Rigidbody2D>();
+        this.animator = this.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        this.previousMovement = this.movement;
+
+        // INPUT
+        this.movement.x = Input.GetAxisRaw("Horizontal");
+        this.movement.y = Input.GetAxisRaw("Vertical");
+        this.movement.Normalize();
+
+        if (this.movement.x > 0 || this.movement.x < 0) this.Flip(this.movement.x > 0);
+
+        this.animator.SetBool("Walking", this.IsWalking);
+    }
+
+    void FixedUpdate()
+    {
+        this.rigidBody.MovePosition(this.rigidBody.position + this.movement * this.movementSpeed * Time.fixedDeltaTime);
+    }
+
+    void Flip(bool right)
+    {
+        if (right != this.facingRight)
+        {
+            Vector3 scale = this.transform.localScale;
+            scale.x *= -1;
+            this.transform.localScale = scale;
+
+            this.facingRight = right;
+        }
+    }
+}
