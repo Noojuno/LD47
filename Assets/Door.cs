55db6e9b2d3d7b50a0daa1b540b68370de67f672
Jono Kemball <Noojuno@users.noreply.github.com>
2020-10-04 05:48:44 +1300

    working button

---
+++ Assets/Door.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public Animator animator;
+
+    public void Toggle(bool open)
+    {
+        Debug.Log(open);
+
+        this.animator.SetBool("Open", open);
+    }
+}
--- Assets/FloorButton.cs
+++ Assets/FloorButton.cs
@@ -2,21 +2,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class FloorButton : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
 
-    public bool triggered = false;
-
     public Sprite inactiveSprite;
     public Sprite activeSprite;
+
+    public bool triggered = false;
+    public UnityEvent<bool> triggerEvent;
+
+    void Start()
+    {
+        if (this.triggerEvent == null)
+            this.triggerEvent = new UnityEvent<bool>();
+    }
 
     void OnTriggerEnter2D(Collider2D col)
     {
         this.triggered = true;
 
         this.spriteRenderer.sprite = activeSprite;
+
+        this.triggerEvent.Invoke(true);
     }
 
     void OnTriggerExit2D(Collider2D col)
@@ -24,5 +34,7 @@ public class FloorButton : MonoBehaviour
         this.triggered = false;
 
         this.spriteRenderer.sprite = inactiveSprite;
+
+        this.triggerEvent.Invoke(false);
     }
 }
